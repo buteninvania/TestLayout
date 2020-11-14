cd2037f95173d4a1d3c6a7f507c200fd6c3f1738
buteninvania <62655847+buteninvania@users.noreply.github.com>
2020-11-14 22:44:21 +0300

    test WebCanape Layout

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+let sliderImgUrl = ["IMG/Slider/slider1.png", "IMG/Slider/slider2.png", "IMG/Slider/slider3.png"]
+let container = document.querySelector(".slider__container")
+
+container.style.backgroundImage = `url(${sliderImgUrl[2]})`
+
